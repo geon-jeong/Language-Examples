678fea6f69a46d9c4628b6e6adec56588d534962
Daniel Geonsi Jeong <40737970+Daniel-G-Jeong@users.noreply.github.com>
2019-12-27 14:31:02 +0900

    [Python] Read Excel File - Add func
    [Python] Read Excel File
    - Add func_read_xlsx_by_cell_name
    - Add func_read_xlsx_by_cell_position

--- lang-FileRead/python/FileRead-Excel.py
+++ lang-FileRead/python/FileRead-Excel.py
@@ -4,26 +4,44 @@ import os
 # >pip install openpyxl
 from openpyxl import load_workbook
 
-
 filename = os.path.abspath('.') + r"\..\DataFiles\data.xlsx"
 
-xlsx_data = load_workbook(filename, data_only=True)
-sheet1 = xlsx_data['Sheet1']
-
-# Cell Data
-print('A1= ', sheet1['A1'].value)
-print('Cell(1,1)= ', sheet1.cell(1,1).value)
-
-# Multi-Cells 
-cells = sheet1['A1':'B2']
-for irow, row in enumerate(cells):
-    for icol, col in enumerate(row):
-        print('[', irow, ',', icol,']=', col.value)
-
-# Rows
-for row in sheet1.rows:
-    print(row)
+def func_read_xlsx_by_cell_name(_file, _sheet, _cell):
+    xlsx_data = load_workbook(_file, data_only=True)
+    sheet1 = xlsx_data[_sheet]
+    value = sheet1[_cell].value
+    xlsx_data.close()
+    return value
     
-# cols
-for col in sheet1.columns:
-    print(col)
+def func_read_xlsx_by_cell_position(_file, _sheet, _row, _col):
+    xlsx_data = load_workbook(_file, data_only=True)
+    sheet1 = xlsx_data[_sheet]
+    value = sheet1.cell(_row, _col).value
+    xlsx_data.close()
+    return value
+
+
+if __name__ == '__main__':
+
+    xlsx_data = load_workbook(filename, data_only=True)
+    sheet1 = xlsx_data['Sheet1']
+
+    # Cell Data
+    print('A1= ', sheet1['A1'].value)
+    print('Cell(1,1)= ', sheet1.cell(1,1).value)
+
+    # Multi-Cells 
+    cells = sheet1['A1':'B2']
+    for irow, row in enumerate(cells):
+        for icol, col in enumerate(row):
+            print('[', irow, ',', icol,']=', col.value)
+
+    # Rows
+    for row in sheet1.rows:
+        print(row)
+    
+    # cols
+    for col in sheet1.columns:
+        print(col)
+    
+    xlsx_data.close()
