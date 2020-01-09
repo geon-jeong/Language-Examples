f22eef7d2288812f05ee7fa4d2e0f9c5f00e9cd4
Daniel Geonsi Jeong <40737970+Daniel-G-Jeong@users.noreply.github.com>
2020-01-09 23:50:30 +0900

    [GUI_WinForm] FCTB Text Editor
    [GUI_WinForm] FCTB Text Editor
    - Install FTCB through Nuget

---
+++ lang-GUI_TextEditor/WinForm/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WindowsFormsApp1.UserControls;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        UCtrl_TextEditor uc_text_editor = new UCtrl_TextEditor();
+        public Form1()
+        {
+            InitializeComponent();
+
+            uc_text_editor.Dock = DockStyle.Fill;
+            this.Controls.Add(uc_text_editor);
+        }
+    }
+}
---
+++ lang-GUI_TextEditor/WinForm/UserControls/UCtrl_TextEditor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FastColoredTextBoxNS;
+
+namespace WindowsFormsApp1.UserControls
+{
+    public partial class UCtrl_TextEditor : UserControl
+    {
+        public FastColoredTextBox FctbEditor { get; set; }
+
+        public UCtrl_TextEditor()
+        {
+            InitializeComponent();
+
+            // set eiditor
+            FctbEditor = new FastColoredTextBox();
+            FctbEditor.Dock = DockStyle.Fill;
+
+            // call back function
+            FctbEditor.TextChanged += new EventHandler<TextChangedEventArgs>(cb_func_Fctb_TextChanged);
+            FctbEditor.KeyPress += new KeyPressEventHandler(this.cb_func_Fctb_KeyPress);
+            FctbEditor.KeyUp += new KeyEventHandler(this.cb_func_Fctb_KeyUp);
+
+            this.Controls.Add(FctbEditor);
+        }
+               
+        void cb_func_Fctb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            e.ChangedRange.ClearFoldingMarkers();
+            e.ChangedRange.SetFoldingMarkers("#<", "#>");
+        }
+
+
+
+        private void cb_func_Fctb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+
+
+        private void cb_func_Fctb_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Alt:
+                    break;
+
+                case Keys.Escape:
+                    break;
+
+                default:
+                    break;
+
+            }
+
+        }
+               
+    } // end of class
+}
