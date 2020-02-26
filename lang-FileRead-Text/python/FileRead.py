83d9a9808dfdc9237286c0ffcd2a8d83ebf52d01
Daniel Geonsi Jeong <40737970+Daniel-G-Jeong@users.noreply.github.com>
2020-02-26 13:39:59 +0900

    [Python] add read text - multifiles in a directory
    [Python] add read text - multifiles in a directory
    Add read multi text files in a directory and consolidate as a one data( string and list)

--- lang-FileRead-Text/python/FileRead.py
+++ lang-FileRead-Text/python/FileRead.py
@@ -2,6 +2,11 @@ import os
 
 filename = os.path.abspath('.') + r"\..\DataFiles\data.csv"
 
+#- open a file and read
+#- open a file and readlines
+#- open a file using with ~ as
+#- open text files under a directory and make it one
+
 # open a file and read
 def func_file_read(_filename):
     f = open( _filename, 'r')
@@ -9,7 +14,7 @@ def func_file_read(_filename):
     f.close()
     return read_data
 
-#open a file ad readlines
+#open a file read readlines
 def func_file_readlines(_filename):
     f = open( _filename, 'r')
     lines = f.readlines()
@@ -25,19 +30,42 @@ def func_file_read_wo_close(_filename):
     return lines
 
 
+
+#- open text files under a directory and make it one
+def func_file_read_and_append(_dir_name):
+    datastr = ""
+    data = []
+    file_name = ['1', '2', '3']
+    for fcnt in file_name:
+        fname = _dir_name+'\%s.%s'%(fcnt,'txt')
+        with open(fname, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                data.append(line)
+                datastr = datastr + line
+                #if(line[-1 == '\n']):
+                #    datastr = datastr + line[:-1]
+    
+    return datastr, data     
+
+
 #
 # Examples to read text-data from a file
 #
-
-rdata = func_file_read(filename)
-print(rdata)
-
-
-rlines = func_file_readlines(filename)
-for iline, line in enumerate(rlines):
-    print('exam1>%4d: %s' %(iline+1, line), end='')
+if __name__ == '__main__':
+    rdata = func_file_read(filename)
+    print(rdata)
 
 
-rlines = func_file_read_wo_close(filename)
-for iline, line in enumerate(rlines):
-    print('exam2>%4d: %s' %(iline+1, line), end='')
+    rlines = func_file_readlines(filename)
+    for iline, line in enumerate(rlines):
+        print('exam1>%4d: %s' %(iline+1, line), end='')
+
+
+    rlines = func_file_read_wo_close(filename)
+    for iline, line in enumerate(rlines):
+        print('exam2>%4d: %s' %(iline+1, line), end='')
+
+    datastr, data = func_file_read_and_append(os.path.abspath('.') + r"\..\DataFiles")    
+    print(datastr)
+    print(data)
